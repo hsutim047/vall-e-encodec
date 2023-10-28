0de2ae1bf326ad5f7c9786299aacd266edbd550b
hsutim047 <hsutim2000@gmail.com>
2023-10-28 22:49:55 +0800

    fix type issue

--- inference_se.py
+++ inference_se.py
@@ -24,10 +24,10 @@ def set_seed(seed):
 
 def nar_decode(model, tokenizer, inputs, batch_code, layer=0):
     base_input = inputs
-    batch_code = np.array(batch_code).transpose((1, 0, 2))
+    batch_code = torch.stack(batch_code, dim=0).permute(1, 0, 2)
     pad_widths = [(0, 0), (0, 7 - batch_code.shape[1]), (0, 0)]
-    batch_code = np.pad(batch_code, pad_widths, mode='constant', constant_values=tokenizer.pad_token_id)
-    base_input["decoder_input_ids"] = batch_code.tolist()
+    batch_code = torch.nn.functional.pad(batch_code, pad_widths, 'constant', value=tokenizer.pad_token_id)
+    base_input["decoder_input_ids"] = batch_code
     decode_nar = model.forward(**base_input).logits
 
     id_range_start, id_range_end = tokenizer.convert_tokens_to_ids(
